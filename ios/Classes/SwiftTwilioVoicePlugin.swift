16dc4d6f9d69d1bf4bb7ec9ca3eef3581556f6e5
ArteChil <artecode66@gmail.com>
2022-03-08 13:20:39 +0800

    Test 5

--- ios/Classes/SwiftTwilioVoicePlugin.swift
+++ ios/Classes/SwiftTwilioVoicePlugin.swift
@@ -496,8 +496,8 @@ public class SwiftTwilioVoicePlugin: NSObject, FlutterPlugin,  FlutterStreamHand
         // from = from.replacingOccurrences(of: "from_firstname", with: "")
         // fromx = fromx.replacingOccurrences(of: "from_lastname", with: "")
         let testmap =  callInvite.customParameters.map { 
-            let giveTest = "\($0["from_firstname"])"
-            from = self.clients[giveTest] ?? ""
+            let giveTest = $0.value["from_firstname"]
+            from = self.clients[giveTest]
         }     
 
         self.sendPhoneCallEvents(description: "Ringing|\(from)|\(callInvite.to)|Incoming\(formatCustomParams(params: callInvite.customParameters))", isError: false)
