a0160848d60b207abc85c64e6b01d833c572fa2f
ArteChil <artecode66@gmail.com>
2022-03-08 19:51:55 +0800

    test 27

--- ios/Classes/SwiftTwilioVoicePlugin.swift
+++ ios/Classes/SwiftTwilioVoicePlugin.swift
@@ -470,7 +470,7 @@ public class SwiftTwilioVoicePlugin: NSObject, FlutterPlugin,  FlutterStreamHand
         UserDefaults.standard.set(Date(), forKey: kCachedBindingDate)
 
         
-        var from:String  = callInvite.callerInfo.description
+        let from:String  = callInvite.callerInfo.description
         var fromx:String = callInvite.from ?? ""
         // from = from.replacingOccurrences(of: "from_firstname", with: "")
         fromx = fromx.replacingOccurrences(of: "from_lastname", with: "")
