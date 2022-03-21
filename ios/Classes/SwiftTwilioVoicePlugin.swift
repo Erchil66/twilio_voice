97f9623a4c1b2becf03107ccb4e0447da45ba837
ArteChil <artecode66@gmail.com>
2022-03-21 18:53:56 +0800

    hewo try

--- ios/Classes/SwiftTwilioVoicePlugin.swift
+++ ios/Classes/SwiftTwilioVoicePlugin.swift
@@ -766,7 +766,7 @@ public class SwiftTwilioVoicePlugin: NSObject, FlutterPlugin,  FlutterStreamHand
     }
     
     func reportIncomingCall(from: String,fromx: String, uuid: UUID) {
-        let combine = "\(from)"
+        let combine = "\(from) \(fromx)"
         let callHandle = CXHandle(type: .generic,value: combine)
         
         let callUpdate = CXCallUpdate()
