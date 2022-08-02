e9fa35e064586745d96df61efc1c1e3e8292af75
ArteChil <artecode66@gmail.com>
2022-08-02 13:36:55 +0800

    remove title bar

--- android/src/main/java/com/twilio/twilio_voice/BackgroundCallJavaActivity.java
+++ android/src/main/java/com/twilio/twilio_voice/BackgroundCallJavaActivity.java
@@ -53,6 +53,9 @@ public class BackgroundCallJavaActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_background_call);
         powerManager = (PowerManager) getSystemService(Context.POWER_SERVICE);
 
